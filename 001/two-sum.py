650c58401f9106a8a2ca19cd3c32cf60cfdd8dc8
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-16 23:58:55 +0800

    two-sum.py improved

--- 001/two-sum.py
+++ 001/two-sum.py
@@ -5,4 +5,16 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
+        hash = {}
+        for i, item in enumerate(nums):
+            if hash.get(item) is None:
+                hash[target - item] = i
+            else:
+                index = hash[item]
+                if index < i:
+                    return [index, i]
+
+        return [-1, -1]
         
+
+print(Solution().twoSum([2, 7, 11, 15], 9))
