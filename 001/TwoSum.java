85c0d817ab9017089061892cc6ce3f61937bea4d
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-17 12:16:30 +0800

    add two-sum java

---
+++ 001/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer idx = map.get(nums[i]);
+
+            if (null == idx) {
+                map.put(target - nums[i], i);
+            } else {
+                if (idx < i) {
+                    res[0] = idx;
+                    res[1] = i;
+                    return re;
+                } 
+            }
+        }
+        return re;
+    }
+}
