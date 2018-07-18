a98e086c523c6e77d72cc2bd77aa9c73bc2ec3ab
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-18 22:02:39 +0800

    ad palindrome .hs .go

---
+++ 009/palindrome-number.go
@@ -0,0 +1,16 @@
+func isPalindrome(x int) bool {
+	if x < 10 && x >= 0 {
+		return true
+	}
+	if x%10 == 0 {
+		return false
+	}
+
+	y := 0
+	for x > y {
+		y = y*10 + x%10
+		x /= 10
+	}
+
+	return x == y || x == y/10
+}
---
+++ 009/palindrome-number.hs
@@ -0,0 +1,2 @@
+isPalindrome ::  Integer -> Bool
+isPalindrome n = show n == reverse (show n)
