bf07eab7dba28eccf40e4d56f4ab3e6c16e89cdb
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-19 17:30:15 +0800

    add one more method

--- 448/find-all-numbers-disappeared-in-an-array.go
+++ 448/find-all-numbers-disappeared-in-an-array.go
@@ -20,6 +20,26 @@ func findDisappearedNumbers(nums []int) []int {
 	return res
 }
 
+// func findDisappearedNumbers(nums []int) []int {
+// 	var res []int
+// 	size := len(nums)
+
+// 	for i := 0; i < size; i++ {
+// 		if nums[i] != nums[nums[i]-1] {
+// 			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+// 			i--
+// 		}
+// 	}
+
+// 	for i := 0; i < size; i++ {
+// 		if (i + 1) != nums[i] {
+// 			res = append(res, (i + 1))
+// 		}
+// 	}
+
+// 	return res
+// }
+
 func main() {
 	nums := []int{4, 3, 2, 7, 8, 2, 3, 1}
 	fmt.Println(findDisappearedNumbers(nums))
