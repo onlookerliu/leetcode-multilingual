c15d91c5853cc8ffd25d4b920e377f8c0e4b31e9
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-20 21:21:35 +0800

    add 094

---
+++ 094/BinaryTreeInOrderTraversal.scala
@@ -0,0 +1,31 @@
+object BinaryTreeInOrderTraversal {
+  def inorderTraversal(root: TreeNode): List[Int] = {
+    var ans = List.empty[Int]
+    def dfs(root: TreeNode): Unit = {
+      if (null == root) return
+      else {
+        dfs(root.left)
+        ans = ans :+ root.value
+        dfs(root.right)
+      }
+    }
+    dfs(root)
+    ans
+  }
+
+  class TreeNode(var _value: Int) {
+    var value: Int = _value
+    var left: TreeNode = null
+    var right: TreeNode = null
+  }
+
+  def main(args: Array[String]): Unit = {
+    val first = new TreeNode(1)
+    val second = new TreeNode(2)
+    val third = new TreeNode(3)
+    second.left = third
+    first.right = second
+    println(inorderTraversal(first))
+  }
+
+}
