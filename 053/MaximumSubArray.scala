6a316752c3cfff33ea46686642a9571e44e3759d
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-20 13:51:57 +0800

    add maxSubArray.scala

--- 053/MaximumSubArray.scala
+++ 053/MaximumSubArray.scala
@@ -1,11 +1,17 @@
 object MaximumSubArray {
   def maxSubArray(nums: Array[Int]): Int = {
     var res = Int.MinValue
-    nums.foldLeft(0)((acc, num) => {
-      val t = if (acc < 0) num else acc + num
+    nums.foldLeft(0) { (acc, num) =>
+      var t = if (acc < 0) num else acc + num
       res = res max t
       t
-    })
+    }
     res
   }
+
+  def main(args: Array[String]): Unit = {
+    var nums = Array(-2,1,-3,4,-1,2,1,-5,4)
+    println(maxSubArray(nums))
+  }
+
 }
