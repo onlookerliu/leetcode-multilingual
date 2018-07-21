5c955a23673d242f335b562c94d3c7a0e5b9ffbe
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-21 18:23:58 +0800

    add 189

---
+++ 189/rotate-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    k %= nums.length;
+    let tmp = [];
+    if (k)
+        tmp = nums.slice(-k);
+    nums.splice(-k, k);
+
+    Array.prototype.unshift.apply(nums, tmp);
+};
