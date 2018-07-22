095b07a299d14239d6df057714aefe15653dd1c2
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-22 19:27:23 +0800

    add 344

---
+++ 344/reverse-string.go
@@ -0,0 +1,7 @@
+func reverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
