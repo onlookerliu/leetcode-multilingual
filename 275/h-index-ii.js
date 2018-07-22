09de82f0c5f215423f3e5e2c1056d6f272df33dc
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-22 18:18:54 +0800

    add 275

---
+++ 275/h-index-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+    var len = citations.length;
+    for (var i = len; ; i--) {
+        if ((!i || citations[len - i] >= i) && (len - i - 1 < 0 || citations[len - i - 1] <= i))
+            return i;
+    }
+};
