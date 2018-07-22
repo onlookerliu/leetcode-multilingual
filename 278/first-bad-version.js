08c812cf056f453eb6fa286a169d6e59324fbbae
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-22 18:20:01 +0800

    add 278

---
+++ 278/first-bad-version.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1, right = n;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (isBadVersion(mid) == true) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    };
+};
