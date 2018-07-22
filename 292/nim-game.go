732a09197663b2493569d234f7fdfa3cfc9dd610
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-22 18:43:56 +0800

    add 292

---
+++ 292/nim-game.go
@@ -0,0 +1,7 @@
+func canWinNim(n int) bool {
+	if n % 4 == 0 {
+		return false
+	} else {
+		return true
+	}
+}
---
+++ 292/nim-game.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function (n) {
+    if (n % 4 == 0)
+        return false;
+    return true;
+};
