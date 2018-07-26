c9708dc91b7987862a665e22c1ec9e794e98d46f
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-26 19:12:27 +0800

    add 687

---
+++ 687/longest-univalue-path.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestUnivaluePath = function (root) {
+    let ans = 0;
+
+    function arrowLength(node) {
+        let left, right, arrowLeft = 0, arrowRight = 0;
+        if (node === null) return 0;
+        left = arrowLength(node.left);
+        right = arrowLength(node.right);
+        if (node.left && node.left.val === node.val)
+            arrowLeft = left + 1;
+        if (node.right && node.right.val === node.val)
+            arrowRight = right + 1;
+        if (arrowLeft + arrowRight > ans)
+            ans = arrowLeft + arrowRight;
+        return Math.max(arrowLeft, arrowRight);
+    }
+
+    arrowLength(root);
+
+    return ans;
+};
