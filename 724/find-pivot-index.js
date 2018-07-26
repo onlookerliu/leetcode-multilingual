9e6f135f80006a83ce2904dbe3f91c49364ddd3b
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-26 20:02:23 +0800

    add 724

--- 724/find-pivot-index.js
+++ 724/find-pivot-index.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var pivotIndex = function (nums) {
-    var suml = 0, sumr = 0, i, length = nums.length;
+    let suml = 0, sumr = 0, len = nums.length;
 
-    for (i = 0; i < length; i++) {
+    for (let i = 0; i < len; i++) {
         sumr += nums[i];
     }
-    for (i = 0; i < length; i++) {
+    for (let i = 0; i < len; i++) {
         if (i > 0) suml += nums[i - 1];
         sumr -= nums[i];
         if (suml === sumr) return i;
