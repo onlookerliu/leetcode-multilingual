aa000f11f68995a2f25773e14860427326733e58
onlookerliu <liu1995@sjtu.edu.cn>
2018-07-27 18:05:28 +0800

    fix error bound condition

--- 005/Solution.java
+++ 005/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
         int maxLen = 0;
         boolean[][] palindromeQ = new boolean[s.length()][s.length()];
         for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i + 1; j < s.length(); j++) {
+            for (int j = i; j < s.length(); j++) {
                 if (s.charAt(i) == s.charAt(j) && (j-i <= 2 || palindromeQ[i+1][j-1])) {
                     palindromeQ[i][j] = true;
                     if (j - i + 1 > maxLen) {
