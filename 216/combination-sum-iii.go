4b322d8f278fd9059755b625259f35831f46512b
onlookerliu <liu1995@sjtu.edu.cn>
2018-08-12 16:19:39 +0800

    add 216

---
+++ 216/combination-sum-iii.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func combinationSum3(k int, n int) [][]int {
+	res := [][]int{}
+	temp := make([]int, k+1)
+	used := make([]bool, 10)
+
+	var dfs func(int, int)
+	dfs = func(idx int, remain int) {
+		if idx == k {
+			if remain < 10 && !used[remain] {
+				temp[idx] = remain
+				t := make([]int, k)
+				copy(t, temp[1:])
+				res = append(res, t)
+			}
+			return
+		}
+
+		for i := temp[idx-1] + 1; i < 10; i++ {
+			if remain-i < i {
+				return
+			}
+			used[i] = true
+			temp[idx] = i
+			dfs(idx+1, remain-i)
+			used[i] = false
+		}
+	}
+
+	dfs(1, n)
+
+	return res
+}
+
+func main() {
+	k, n := 3, 9
+	fmt.Println(combinationSum3(k, n))
+}
