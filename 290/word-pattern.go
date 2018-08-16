d997ef8bc68ffbb275304dec58210e1c7841eaa2
onlookerliu <liu1995@sjtu.edu.cn>
2018-08-16 15:51:26 +0800

    add 290

---
+++ 290/word-pattern.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func wordPattern(pattern string, str string) bool {
+	ps := strings.Split(pattern, "")
+	ss := strings.Split(str, "")
+
+	if len(ps) != len(ss) {
+		return false
+	}
+
+	return isMatch(ps, ss) && isMatch(ss, ps)
+}
+
+func isMatch(s1, s2 []string) bool {
+	length := len(s1)
+
+	m := make(map[string]string, length)
+
+	var i int
+	var w string
+	var ok bool
+
+	for i = 0; i < length; i++ {
+		if w, ok = m[s1[i]]; ok {
+			if w != s2[i] {
+				return false
+			}
+		} else {
+			m[s1[i]] = s2[i]
+		}
+	}
+
+	return true
+}
+
+func main() {
+	pattern := "abba"
+	str := "dog cat cat fish"
+	fmt.Println(wordPattern(pattern, str))
+}
