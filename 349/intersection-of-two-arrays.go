31b55ca891669f8de7f3854d5747fd3ae43a7513
onlookerliu <liu1995@sjtu.edu.cn>
2018-08-30 16:22:16 +0800

    add 349

--- 349/intersection-of-two-arrays.go
+++ 349/intersection-of-two-arrays.go
@@ -1,3 +1,27 @@
-func intersection(nums1 []int, nums2 []int) []int {
+func intersection(a1, a2 []int) []int {
+	res := []int{}
+	m1 := getInts(a1)
+	m2 := getInts(a2)
 
+	if len(m1) > len(m2) {
+		m1, m2 = m2, m1
+	}
+
+	for n := range m1 {
+		if m2[n] {
+			res = append(res, n)
+		}
+	}
+
+	return res
+}
+
+func getInts(a []int) map[int]bool {
+	res := make(map[int]bool, len(a))
+
+	for i := range a {
+		res[a[i]] = true
+	}
+
+	return res
 }
