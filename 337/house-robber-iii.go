a0dc2a39de382098c2e5502f582bedb397d4628d
onlookerliu <liu1995@sjtu.edu.cn>
2018-08-30 15:16:29 +0800

    add 337

---
+++ 337/house-robber-iii.go
@@ -0,0 +1,31 @@
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rob(root *TreeNode) int {
+	var dfs func(*TreeNode) (int, int)
+	// a 抢劫 root 节点时的最大值
+	// b 不抢劫 root 节点时的最大值
+	dfs = func(root *TreeNode) (a, b int) {
+		if root == nil {
+			return 0, 0
+		}
+		la, lb := dfs(root.Left)
+		ra, rb := dfs(root.Right)
+		a = root.Val + lb + rb
+		b = max(la, lb) + max(ra, rb)
+		return a, b
+	}
+
+	a, b := dfs(root)
+	return max(a, b)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
