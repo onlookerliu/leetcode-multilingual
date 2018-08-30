9dd0e917a002b694b92e030d3c950b19571d0a54
onlookerliu <liu1995@sjtu.edu.cn>
2018-08-30 15:52:27 +0800

    add 343

---
+++ 343/integer-break.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func integerBreak(n int) int {
+	if n == 2 {
+		return 1
+	}
+
+	if n == 3 {
+		return 2
+	}
+
+	switch n % 3 {
+	case 0:
+		return pow3(n / 3)
+	case 1:
+		return 4 * pow3(n/3-1)
+	default:
+		return 2 * pow3(n/3)
+	}
+}
+
+func pow3(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	res := pow3(n >> 1)
+	if n&1 == 0 {
+		return res * res
+	}
+
+	return res * res * 3
+}
+
+func main() {
+	n := 10
+	fmt.Println(integerBreak(n))
+}
