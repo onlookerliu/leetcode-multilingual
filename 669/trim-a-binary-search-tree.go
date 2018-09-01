800b5f1f72f6849650d7ace86ef37aa1a8a76cb4
onlookerliu <liu1995@sjtu.edu.cn>
2018-09-01 23:52:24 +0800

    add 669

---
+++ 669/trim-a-binary-search-tree.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+
+	if R < root.Val {
+		return trimBST(root.Left, L, R)
+	}
+
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
