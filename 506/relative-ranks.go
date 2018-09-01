a971faaaaf3dbb15130ff66b3871661b54fdcd66
onlookerliu <liu1995@sjtu.edu.cn>
2018-09-01 19:51:27 +0800

    add 506

---
+++ 506/relative-ranks.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func findRelativeRanks(nums []int) []string {
+	res := make([]string, len(nums))
+	as := make(athletes, len(nums))
+
+	for i := range nums {
+		as[i] = athlete{
+			sorce: nums[i],
+			index: i,
+		}
+	}
+
+	sort.Sort(as)
+
+	for i, a := range as {
+		switch i {
+		case 0:
+			res[a.index] = "Gold Medal"
+		case 1:
+			res[a.index] = "Silver Medal"
+		case 2:
+			res[a.index] = "Bronze Medal"
+		default:
+			res[a.index] = strconv.Itoa(i + 1)
+		}
+	}
+
+	return res
+}
+
+type athlete struct {
+	sorce, index int
+}
+
+type athletes []athlete
+
+func (a athletes) Len() int {
+	return len(a)
+}
+
+func (a athletes) Less(i, j int) bool {
+	return a[i].sorce > a[j].sorce
+}
+
+func (a athletes) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func main() {
+	// [5, 4, 3, 2, 1]
+	nums := []int{5, 4, 3, 2, 1}
+	fmt.Println(findRelativeRanks(nums))
+}
