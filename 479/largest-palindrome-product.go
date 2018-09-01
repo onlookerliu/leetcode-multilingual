9f5f234bc608460ea41aaceef000c0b376e06e46
onlookerliu <liu1995@sjtu.edu.cn>
2018-09-01 15:02:00 +0800

    add 479

---
+++ 479/largest-palindrome-product.go
@@ -0,0 +1,4 @@
+func largestPalindrome(n int) int {
+	res := []int{9, 987, 123, 597, 677, 1218, 877, 475}
+	return res[n-1]
+}
---
+++ 479/largest-palindrome-product.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var largestPalindrome = function(n) {
+    let res = [9, 987, 123, 597, 677, 1218, 877, 475];
+    return res[n-1];
+};
