cbc15e8709f7204ef4f5729f64305a07c7836369
onlookerliu <liu1995@sjtu.edu.cn>
2018-09-11 09:52:47 +0800

    add 035

---
+++ 035/SearchInsertPosition.scala
@@ -0,0 +1,21 @@
+package leetcode
+
+object SearchInsertPosition {
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+    var i: Int = 0
+    while (i < nums.length && nums(i) <= target) {
+      if (nums(i) == target) {
+        return i
+      }
+      i += 1
+    }
+    return i
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums = Array(1, 3, 5, 6)
+    val target = 5
+    println(searchInsert(nums, target))
+  }
+
+}
