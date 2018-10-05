56b8ca6922888db382efa8b4a0a58bfa28b49bb2
onlookerliu <liu1995@sjtu.edu.cn>
2018-10-05 12:33:13 +0800

    add one more solution

--- 801/Solution.cpp
+++ 801/Solution.cpp
@@ -16,11 +16,38 @@ class Solution {
         }
 
         if (A[i] > A[i-1] && B[i] > B[i-1])
-            dfs(A, B, i+1, c, ans)
+            dfs(A, B, i+1, c, ans);
         if (A[i] > B[i-1] && B[i] > A[i-1]) {
             swap(A[i], B[i]);
             dfs(A, B, i+1, c+1, ans);
             swap(A[i], B[i]);
         }
+    }
+};
+
+
+class Solution {
+    public:
+    int minSwap(vector<int>& A, vector<int>& B) {
+        const int n = A.size();
+        vector<int> keep(n, INT_MAX);
+        vector<int> swap(n, INT_MAX);
+
+        keep[0] = 0;
+        swap[0] = 1;
+
+        for (int i = 1; i < n; ++i) {
+            if (A[i] > A[i-1] && B[i] > B[i-1]) {
+                keep[i] = keep[i-1];
+                swap[i] = swap[i-1] + 1;
+            }
+
+            if (B[i] > A[i-1] && A[i] > B[i-1]) {
+                swap[i] = min(swap[i], keep[i-1]+1);
+                keep[i] = min(keep[i], swap[i-1]);
+            }
+        }
+
+        return min(keep.back(), swap.back());
     }
 };
