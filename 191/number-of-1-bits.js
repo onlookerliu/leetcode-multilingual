ade2434d8635fd390e892cee0f55cb20ceed747e
onlookerliu <liu1995@sjtu.edu.cn>
2018-10-11 09:46:42 +0800

    add 191

--- 191/number-of-1-bits.js
+++ 191/number-of-1-bits.js
@@ -13,3 +13,12 @@ var hammingWeight = function (n) {
     }
     return count;
 };
+
+var hammingWeight = function (n) {
+    let count = 0;
+    while (n != 0) {
+        count++;
+        n = n & (n - 1);
+    }
+    return count;
+};
--- 191/number-of-1-bits.py
+++ 191/number-of-1-bits.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def hammingWeight(self, n):
         """
         :type n: int
@@ -10,4 +10,17 @@ class Solution(object):
             if 1 == tmp:
                 count += 1
             n = (n - tmp) / 2
+        return count
+
+
+class Solution:
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n != 0:
+            count += 1
+            n = n & (n - 1)
         return count
