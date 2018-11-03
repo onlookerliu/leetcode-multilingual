65f5fae1c9a869c8943684862fd4ba3d29854c0d
onlookerliu <liu1995@sjtu.edu.cn>
2018-11-03 16:56:07 +0800

    test emacs magit use

--- 007/reverse-integer.js
+++ 007/reverse-integer.js
@@ -18,6 +18,7 @@ var reverse = function (x) {
         return ans;
 };
 
+// another solution
 var reverse = function (x) {
     let y = x < 0 ? -x : x, re = 0, maxint = 2147483647;
     while (y > 0) {
